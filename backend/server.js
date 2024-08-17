4aff88b5080f125a9d86b12b4478f268b16458ac
Pnandi <paramitanandi65@gmail.com>
2024-08-17 20:58:53 +0530

    Api end point setup, Create all list, add and remove api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 //app config
 const app = express()
@@ -9,6 +11,13 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+//db connection
+connectDB();
+
+//api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+
 app.get("/",(req, res)=>{
 res.send("Api Working")
 })
@@ -16,3 +25,4 @@ res.send("Api Working")
 app.listen(port,()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
+
